1b8b4fd3d3b1da464f03261991b8cf43ee7ff0f1
Sen Madukkamoottil <sentvla@gmail.com>
2025-07-02 14:10:26 -0400

    Promise all and all settled

--- src/creating.mjs
+++ src/creating.mjs
@@ -46,9 +46,38 @@ export function xhr(){
 }
 
 export function allPromises(){
+    const categories = axios('http://localhost:3000/itemCategories');
+    const status = axios('http://localhost:3000/orderStatuses');
+    const userType = axios('http://localhost:3000/userTypes');
+
+    Promise.all([categories, status, userType])
+        .then(([categories, status, userType]) => {
+        setText(JSON.stringify(categories.data));
+        appendText(JSON.stringify(status.data));
+        appendText(JSON.stringify(userType.data));
+    }).catch((response) => {
+        setText(response);
+    });
 }
 
 export function allSettled(){
+    const categories = axios('http://localhost:3000/itemCategories1');
+    const status = axios('http://localhost:3000/orderStatuses');
+    const userType = axios('http://localhost:3000/userTypes');
+
+    Promise.allSettled([categories, status, userType])
+        .then((value) => {
+        let result = value.map(data => {
+            if (data.status === 'fulfilled') {
+                return `Fullfilled - ${JSON.stringify(data.value.data[0])}`;
+            } else {
+                return `Rejected - ${data.reason}`;
+            }
+        });
+        setText(result);
+    }).catch((response) => {
+        setText(response);
+    });
 }
 
 export function race(){
